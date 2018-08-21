6b148d415ea087be04a1f0c3227a886d856a2e0c
RangerZz <zhangrangerzz@yahoo.com>
2018-08-21 17:15:37 +0800

    fix sh

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 if [ $# -lt  1 ]; then
-    echo "$0 <commit message>"
-    exit 1
+echo "$0 <commit message>"
+exit 1
 fi
 msg="$1"
 git commit -m "$msg"
 if [ $? -ne 0 ]; then
-    echo "Commit failed"
-    exit 1
+echo "Commit failed"
+exit 1
 fi
 git push origin master
 if [ $? -ne 0 ]; then
-    echo "Push failed"
+echo "Push failed"
 fi
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 # Build the project.
